608b92b6908788fb91f0852f130c5e2ba4937794
Yulia Khrenenko <romanova_jv@mail.ru>
2020-08-14 14:43:26 +0300

    PostManager added

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,16 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    private Post[] search(int ownerId, String domain, String query, int ownersOnly, int count, int offset, boolean extended) {
+        //TODO: add logic
+        return null;
+    }
+
+    private void delete(int ownerId, int postId) {
+        //TODO: add logic
+    }
+}
