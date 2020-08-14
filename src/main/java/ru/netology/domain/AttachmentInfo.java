e7424cce664a6a3bf11cd9874efd18a451260afe
Yulia Khrenenko <romanova_jv@mail.ru>
2020-08-14 13:51:43 +0300

    Version 1

---
+++ src/main/java/ru/netology/domain/AttachmentInfo.java
@@ -0,0 +1,8 @@
+package ru.netology.domain;
+
+public class AttachmentInfo {
+    private String imageUrl;
+    private String articleUrl;
+    private String videoUrl;
+    private String linkUrl;
+}
---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,150 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;
+    private int userId;
+    private String text;
+    private CommentsInfo commentsInfo;
+    private LikesInfo likesInfo;
+    private RepostsInfo repostsInfo;
+    private int date;
+    private ViewsInfo viewsInfo;
+    private AttachmentInfo attachmentInfo;
+    private boolean isFavorite;
+    private int fromId;
+    private String copyright;
+    private String postType;
+    private GeoInfo geoInfo;
+    private boolean canDelete;
+    private boolean canEdit;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public CommentsInfo getCommentsInfo() {
+        return commentsInfo;
+    }
+
+    public void setCommentsInfo(CommentsInfo commentsInfo) {
+        this.commentsInfo = commentsInfo;
+    }
+
+    public LikesInfo getLikesInfo() {
+        return likesInfo;
+    }
+
+    public void setLikesInfo(LikesInfo likesInfo) {
+        this.likesInfo = likesInfo;
+    }
+
+    public RepostsInfo getRepostsInfo() {
+        return repostsInfo;
+    }
+
+    public void setRepostsInfo(RepostsInfo repostsInfo) {
+        this.repostsInfo = repostsInfo;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public ViewsInfo getViewsInfo() {
+        return viewsInfo;
+    }
+
+    public void setViewsInfo(ViewsInfo viewsInfo) {
+        this.viewsInfo = viewsInfo;
+    }
+
+    public AttachmentInfo getAttachmentInfo() {
+        return attachmentInfo;
+    }
+
+    public void setAttachmentInfo(AttachmentInfo attachmentInfo) {
+        this.attachmentInfo = attachmentInfo;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public int getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(int fromId) {
+        this.fromId = fromId;
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        this.copyright = copyright;
+    }
+
+    public String getPostType() {
+        return postType;
+    }
+
+    public void setPostType(String postType) {
+        this.postType = postType;
+    }
+
+    public GeoInfo getGeoInfo() {
+        return geoInfo;
+    }
+
+    public void setGeoInfo(GeoInfo geoInfo) {
+        this.geoInfo = geoInfo;
+    }
+
+    public boolean isCanDelete() {
+        return canDelete;
+    }
+
+    public void setCanDelete(boolean canDelete) {
+        this.canDelete = canDelete;
+    }
+
+    public boolean isCanEdit() {
+        return canEdit;
+    }
+
+    public void setCanEdit(boolean canEdit) {
+        this.canEdit = canEdit;
+    }
+
+}
+
